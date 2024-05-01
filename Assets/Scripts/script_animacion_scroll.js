a9893db6d484b1506ae76148eb6fdc22d98f30f0
JcManuel44 <163625325+JcManuel44@users.noreply.github.com>
2024-05-01 00:58:37 -0400

    Update script_animacion_scroll.js

--- Assets/Scripts/script_animacion_scroll.js
+++ Assets/Scripts/script_animacion_scroll.js
@@ -1,12 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("El DOM ha sido cargado correctamente.");
-
-    // Agregar un event listener para el evento de scroll
     window.addEventListener("scroll", function() {
-        console.log("La página se ha desplazado.");
-    });
-
-    // Inicializar AOS
     AOS.init();
+    });
 });
 

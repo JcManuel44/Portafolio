89cd001740a81128d757573e27cf903154fe4b1f
JcManuel44 <163625325+JcManuel44@users.noreply.github.com>
2024-05-01 00:36:28 -0400

    Add files via upload

---
+++ Assets/Scripts/script_correo_elelctronico.js
@@ -0,0 +1,48 @@
+function enviarMensaje() {
+   emailjs.init('vYFSLmk58NrjMza_t');
+
+   var txt_nombre = document.getElementById('id_txt_nombre').value;
+   var txt_empresa = document.getElementById('id_txt_empresa').value;
+   var txt_correo = document.getElementById('id_txt_correo').value;
+   var txt_mensaje = document.getElementById('id_txt_mensaje').value;
+
+   if (txt_nombre === '' || txt_empresa === '' || txt_correo === '' || txt_mensaje === '') {
+      alert('Por favor, complete todos los campos.');
+      return false;
+   } else {
+
+      var maxCaracteres_Nombre = 60;
+      var maxCaracteres_Empresa = 70;
+      var maxCaracteres_Correo = 70;
+      var maxCaracteres_Mensaje = 300;
+
+      if (txt_nombre.length > maxCaracteres_Nombre || txt_empresa.length > maxCaracteres_Empresa || txt_correo.length > maxCaracteres_Correo || txt_mensaje.length > maxCaracteres_Mensaje) {
+         alert('Los campos exceden el límite de ' + maxCaracteres + ' caracteres.');
+         return false;
+      } else {
+      }
+   }
+   return true;
+   emailjs.send("service_se3xvqg", "template_fi2thvf", {
+      txt_nombre: txt_nombre,
+      txt_empresa: txt_empresa,
+      txt_correo: txt_correo,
+      txt_mensaje: txt_mensaje
+   }).then(function (response) {
+      console.log('Mensaje enviado correctamente', response);
+      console.log('---------------------------------------');
+      console.log('Nombre:', txt_nombre);
+      console.log('Empresa:', txt_empresa);
+      console.log('Correo:', txt_correo);
+      console.log('Mensaje:', txt_mensaje);
+      alert('Mensaje enviado correctamente');
+   })
+      .catch(function (error) {
+         console.error('Error al enviar el mensaje', error);
+         alert('Error al enviar el mensaje');
+      });
+}
+
+
+
+
